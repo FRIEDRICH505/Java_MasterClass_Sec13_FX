cd98af66fa950da0aa742224741feaaf95e8759f
FRIEDRICH505 <67607043+FRIEDRICH505@users.noreply.github.com>
2021-09-19 17:44:35 +0200

    Add files via upload

---
+++ JavaFX_Challenge/src/module-info.java
@@ -0,0 +1,8 @@
+module JavaFX.Challenge {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.xml;
+    requires java.desktop;
+    opens sample.datamodel;
+    opens sample;
+}
